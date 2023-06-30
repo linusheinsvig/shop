9c84df7c178a40aed4efc82344b4ae45126105c1
Linus Heinsvig <heinsvig@icloud.com>
2023-06-30 16:37:09 +0000

    Add checkout templates and styling

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
