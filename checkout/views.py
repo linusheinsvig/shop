e8f5474feb0f2d94d732afb93abd9e5232582889
Linus Heinsvig <heinsvig@icloud.com>
2023-07-02 13:52:17 +0000

    Setup stripe to work

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+import stripe
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     
     current_bag = bag_contents(request)
     total = current_bag['total']
+    
+    if total <= 0:
+        return render(request, 'checkout/error.html', {'error_message': 'Total must be greater than zero.'})
+    
     stripe_total = round(total * 100)
-
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NNFEzChlBYEIuSVgX3DT2BiKOEp38EOS03eFWuijU6XwsQ0F6Fc9C3GxgKABY7LxE38y5aY0Yd7sqnNIamjefiW00s5aJpn9T',
-        'client_secret': 'test client secret',
-    }
-
-    return render(request, template, context)
+    stripe.api_key = stripe_secret_key
+    
+    try:
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        print(intent)
+        
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+        
+        return render(request, template, context)
+    
+    except stripe.error.InvalidRequestError as e:
+        print(f"Stripe Error: {str(e)}")
+        return render(request, 'checkout/error.html', {'error_message': 'An error occurred during payment processing.'})
